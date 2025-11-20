db5c21a170cbe647b003a916c2419e2386278390
Thalgit <steeladres@outlook.com>
2025-11-20 16:06:51 +0100

    verbose + flag filter

--- main.py
+++ main.py
@@ -11,13 +11,24 @@ from google.genai import types
 client = genai.Client(api_key=api_key)
 
 def main():
+    
     #geen prompt = exit, len 1 want 0 van argv is script naam, niet prompt
     if len(sys.argv) == 1:
         print("Error, prompt required")
         sys.exit(1)
-    else:
-        prompt = " ".join(sys.argv[1:])
     
+    #prompt bouwen, flag filter
+    else:
+        prompt_parts = []
+        for i in sys.argv[1:]:
+            if not i.startswith("--"):
+                prompt_parts.append(i)
+        prompt = " ".join(prompt_parts)
+    
+    #verbose ja/nee
+    verbose = "--verbose" in sys.argv
+
+    #history
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
@@ -27,9 +38,12 @@ def main():
         contents = messages,
     )
 
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
 
 if __name__ == "__main__":
     main()
