c55d32ad443a239843e668a29886fd3a6ca5b568
Thalgit <steeladres@outlook.com>
2025-11-25 17:26:38 +0100

    running files

---
+++ functions/run_python.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    full_path = os.path.join(working_directory, file_path)
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if args is None:
+        args = []        
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        completed_process = subprocess.run(
+            ["python", abs_file_path, *args],
+            cwd=abs_working_dir,
+            capture_output=True,
+            text=True,
+            timeout=30,
+        )
+        
+        stdout = completed_process.stdout
+        stderr = completed_process.stderr
+        code = completed_process.returncode
+
+        if stdout == "" and stderr == "":
+            return "No output produced."
+
+        result = f'STDOUT: {stdout}\nSTDERR: {stderr}'
+        
+        if code != 0:
+            result += f"\nProcess exited with code {code}"
+
+        return result
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
