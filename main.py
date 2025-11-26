d07914de91def8984eec9176f81fca01ca5e88df
Thalgit <steeladres@outlook.com>
2025-11-26 14:58:29 +0100

    declerations, function calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import sys
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -33,17 +34,36 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
+    system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
+
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001", 
         contents = messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
     )
 
     if verbose:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    print(response.text)
+
+    if not response.function_calls:
+        print(response.text)
+        return
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
         
 
 if __name__ == "__main__":
